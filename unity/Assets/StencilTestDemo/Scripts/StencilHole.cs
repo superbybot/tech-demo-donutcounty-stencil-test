bad620736d7cc447cf1681093012bcf26f8e4030
Byron Bautista <superbybot@gmail.com>
2024-11-11 04:19:49 +0900

    wip stencil test

---
+++ unity/Assets/StencilTestDemo/Scripts/StencilHole.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace StencilTestDemo.Scripts
+{
+    public class StencilHole : MonoBehaviour
+    {
+        [SerializeField] private Collider _groundCollider;
+        
+    }
+}
---
+++ unity/Assets/StencilTestDemo/Scripts/StencilTestLifetimeScope.cs
@@ -0,0 +1,9 @@
+using VContainer.Unity;
+
+namespace StencilTestDemo.Scripts
+{
+    public class StencilTestLifetimeScope : LifetimeScope
+    {
+        
+    }
+}
